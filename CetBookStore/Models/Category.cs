50c2ff4ea3d67efb61710fb978cc8390d032e46f
Huseyin Simsek <huseyinsimsek@gmail.com>
2020-04-13 19:22:25 +0300

    Add project files.

---
+++ CetBookStore/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CetBookStore.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [StringLength(100,MinimumLength =3)]
+        public string Name { get; set; } // nvarchar(100) 2GB
+
+        public virtual List<Book> Books { get; set; }
+    }
+}
